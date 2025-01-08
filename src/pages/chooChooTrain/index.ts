e84ded421ebb0d745ec275c67a732bd29c1a3ef7
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-01-08 23:35:44 +0100

    Add initial structure for "Choo choo train" example

---
+++ src/pages/chooChooTrain/index.ts
@@ -0,0 +1,10 @@
+import { Application } from 'pixi.js';
+
+const app = new Application();
+
+(async () =>
+{
+    await app.init({ background: '#021f4b', resizeTo: window });
+
+    document.body.appendChild(app.canvas);
+})();
